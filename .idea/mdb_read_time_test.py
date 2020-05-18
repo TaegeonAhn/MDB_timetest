badd0cee01fdcaef451a6fbe301351c8175332b4
atg0606 <atg0606@cenacle.com>
2020-05-18 11:09:36 +0900

    mdb read/insert test

---
+++ .idea/mdb_read_time_test.py
@@ -0,0 +1,16 @@
+import pyodbc
+import time
+
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\타사B migration test\챠트자료\월별5.mdb;')
+cursor = conn.cursor()
+
+start = time.time() #시작 시간 저장
+
+cursor.execute('select * from 환자정보')
+
+for row in cursor.fetchall():
+    print (row)
+
+
+print("time : ", time.time() - start ) #현재시간 - 시작시간 = 실행 시간
+conn.close()
---
+++ github.py
@@ -0,0 +1,4 @@
+git config user.name "atg0606"
+
+git config user.email "atg0606@cenacle.com"
+
